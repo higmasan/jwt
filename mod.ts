68ffe9a8f0420c7fac25877f5ce2bacd86ec68d2
yoshiyasu1111 <aris_and_teres_31415926535@yahoo.co.jp>
2025-02-07 08:03:53 +0000

    パスの指定の仕方を変更した

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import { decodeBase64Url, encodeBase64Url } from 'https://deno.land/std@0.224.0/encoding/base64url.ts'
+import { decodeBase64Url, encodeBase64Url } from "jsr:@std/encoding@1.0"
 
 interface Payload {
   iss?: string,             // Issuer
